c01cf63387ec586826555ce7e142874bf1374a8a
USRVI-LC3 <USRVI-LC3@LC060300.duoc.acad>
2023-09-13 16:22:19 -0300

    ejemplo coleccion

---
+++ src/Unidad2/EjemploColeccion/Coleccion.java
@@ -0,0 +1,23 @@
+
+package Unidad2.EjemploColeccion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author USRVI-LC3
+ */
+public class Coleccion {
+    
+    public static void main(String[] args) {
+        
+        
+        ArrayList<String> listaNombres = new ArrayList<>();
+        
+        
+
+        
+    }
+    
+}
