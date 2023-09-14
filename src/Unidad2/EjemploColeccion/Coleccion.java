f5d248b5cb168284ecd31ebc542b8d28c1a51acb
USRVI-LC3 <USRVI-LC3@LC060300.duoc.acad>
2023-09-14 17:56:27 -0300

    ejercicio colecciones (Auto)

--- src/Unidad2/EjemploColeccion/Coleccion.java
+++ src/Unidad2/EjemploColeccion/Coleccion.java
@@ -2,7 +2,7 @@
 package Unidad2.EjemploColeccion;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  *
@@ -13,12 +13,35 @@ public class Coleccion {
     public static void main(String[] args) {
         
         
-        ArrayList<String> listaNombres = new ArrayList<>();
+        ArrayList<String> listaNombres = new ArrayList<>(); //guardo Strings
         listaNombres.add("Juan");
-        listaNombres.add("Matías"); 
+        listaNombres.add("Matias"); 
         listaNombres.add("Armando Casas");
         listaNombres.add("XZEBA");
-                
+        
+        System.out.println(listaNombres);
+        
+        //fore + tab
+        for (String dato : listaNombres) {
+            System.out.println(dato);
+        }
+        System.out.println("");
+        //AHORA AUTOS
+        System.out.println("******AUTOS*******");
+        
+        ArrayList<Auto>listaAutos = new ArrayList<>(); //guardo Autos
+        
+        Auto auto1 = new Auto("KIA", "MORNING", 2020);
+        Auto auto2 = new Auto("FIAT","600",1963);
+        
+        listaAutos.add(auto1);
+        listaAutos.add(auto2);
+        
+        System.out.println(listaAutos);
+        
+        for (Auto auto : listaAutos) {
+            System.out.println(auto.getMarca());
+        }
         
     }
     
